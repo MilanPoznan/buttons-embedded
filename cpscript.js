5ac89f3a326ed4bc10c7d5638b95c9fdb9f507a3
MilanPoznan <poznan.milan@gmail.com>
2016-10-05 14:22:51 +0200

    Update cpscript.js

--- cpscript.js
+++ cpscript.js
@@ -88,3 +88,49 @@
     picker2.on("exit", on_exit2);
     picker2.on("fit", on_fit2);
     picker2.on("create", on_create2);
+
+// ======= Change Arrow color ====
+var picker = new CP(document.getElementById('changeArrow'));
+
+    function on_change3(v, instance) {
+        instance.target.value = '#' + v;
+        document.getElementById("g1").style.fill = '#' + v;
+        console.log('change');
+    }
+
+    function on_start3() {
+        console.log('start');
+    }
+
+    function on_drag3() {
+        console.log('drag');
+    }
+
+    function on_stop3() {
+        console.log('stop');
+    }
+
+    function on_enter3() {
+        console.log('enter');
+    }
+
+    function on_exit3() {
+        console.log('exit');
+    }
+
+    function on_fit3() {
+        console.log('fit');
+    }
+
+    function on_create3(v, instance) {
+        console.log('create');
+    }
+
+    picker.on("change", on_change3);
+    picker.on("start", on_start3);
+    picker.on("drag", on_drag3);
+    picker.on("stop", on_stop3);
+    picker.on("enter", on_enter3);
+    picker.on("exit", on_exit3);
+    picker.on("fit", on_fit3);
+    picker.on("create", on_create3);
