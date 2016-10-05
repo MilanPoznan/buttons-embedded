cc46b3b5973f86a587ef9af67d1dcb2e42d3a8bd
MilanPoznan <poznan.milan@gmail.com>
2016-10-05 13:46:47 +0200

    Update cpscript.js

--- cpscript.js
+++ cpscript.js
@@ -3,7 +3,7 @@
 
     function on_change(v, instance) {
         instance.target.value = '#' + v;
-        document.getElementById("Layer_1_butt").style.fill = '#' + v;
+        document.getElementById("Layer_1_butt").style.fill = '#' + v !important;
         console.log('change');
     }
 
